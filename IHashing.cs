4bdf1b5edea85d104da8d162cefd6f33cc106085
Gabriel Oliveira <atacckover@gmail.com>
2025-08-18 19:45:00 -0300

    Adicionar arquivos de projeto.

---
+++ IHashing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IHashing<T> where T : IRegistro<T>, new()
+{
+    bool Incluir(T novoDado);
+    bool Excluir(T dado);
+    bool Existe(T dado, out int onde);
+    List<T> Conteudo();
+}
